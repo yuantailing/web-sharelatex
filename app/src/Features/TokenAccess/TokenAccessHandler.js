ec946054aebfb0b36269dabd23528b3ed553f6f9
Tailing Yuan <yuantailing@gmail.com>
2021-01-04 17:03:14 +0800

    Revert "allow read-only public share without v1 API"
    This reverts commit b69737c0f7a9d5da72f176bf448eafba54adb401.

--- app/src/Features/TokenAccess/TokenAccessHandler.js
+++ app/src/Features/TokenAccess/TokenAccessHandler.js
@@ -254,17 +254,17 @@ const TokenAccessHandler = {
   },
 
   getV1DocPublishedInfo(token, callback) {
-    return Project.findOne(
-      { publicAccesLevel: 'tokenBased', 'tokens.readOnly': token },
-      (error, project) => {
-        if (error != null) return callback(error)
-        if (
-          project != null &&
-          project.publicAccesLevel === 'tokenBased' &&
-          project.tokens.readOnly === token
-        )
-          return callback(null, { allow: true })
-        return callback(null, { allow: false })
+    // default to allowing access
+    if (!Settings.apis.v1 || !Settings.apis.v1.url) {
+      return callback(null, { allow: true })
+    }
+    V1Api.request(
+      { url: `/api/v1/sharelatex/docs/${token}/is_published` },
+      function(err, response, body) {
+        if (err != null) {
+          return callback(err)
+        }
+        callback(null, body)
       }
     )
   },
